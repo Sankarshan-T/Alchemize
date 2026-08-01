999cba013b8a2317bffcb9a95b35a0cab8252036
theutkarsh8939 <utkarshchandel2012@gmail.com>
2026-08-01 17:48:34 +0530

    Cookies oops!

--- src/routes/auth/+page.server.ts
+++ src/routes/auth/+page.server.ts
@@ -3,9 +3,9 @@ import { redirect } from "@sveltejs/kit"
 import {scopes} from "$lib/utils"
 import type {PageServerLoad} from "./$types"
 import crypto from "crypto"
-export const load = ({cookie}) => {
+export const load = ({cookies}) => {
     const state = crypto.randomBytes(16).toString("hex")
-    cookie.set("oauth_state", state, {path: "/", httpOnly: true, sameSite: "strict", secure: true, maxAge: 300})
+    cookies.set("oauth_state", state, {path: "/", httpOnly: true, sameSite: "strict", secure: true, maxAge: 300})
     const url = "https://auth.hackclub.com/oauth/authorize?client_id=" + PUBLIC_HACKCLUB_AUTH + "&redirect_uri=" + PUBLIC_HACKCLUB_REDIRECT + "&response_type=code&scope=" + scopes + "&state=" + state
     return redirect(308, url)
 }
