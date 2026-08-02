0874be84658524e844b25b6cdaf6634dd0a3744e
CoolCream <mahasankarshant@gmail.com>
2026-08-02 17:03:49 +0530

    fixed a small dashboard login issue

--- src/routes/auth/+page.server.ts
+++ src/routes/auth/+page.server.ts
@@ -1,11 +1,11 @@
-import {PUBLIC_HACKCLUB_AUTH, PUBLIC_HACKCLUB_REDIRECT} from "$env/static/public"
+import { PUBLIC_HACKCLUB_AUTH, PUBLIC_HACKCLUB_REDIRECT } from "$env/static/public"
 import { redirect } from "@sveltejs/kit"
-import {scopes} from "$lib/utils"
-import type {PageServerLoad} from "./$types"
+import { scopes } from "$lib/utils"
+import type { PageServerLoad } from "./$types"
 import crypto from "crypto"
-export const load = ({cookies}) => {
+export const load = ({ cookies }) => {
     const state = crypto.randomBytes(16).toString("hex")
-    cookies.set("oauth_state", state, {path: "/", httpOnly: true, sameSite: "strict", secure: true, maxAge: 300})
+    cookies.set("oauth_state", state, { path: "/", httpOnly: true, sameSite: "lax", secure: true, maxAge: 300 })
     const url = "https://auth.hackclub.com/oauth/authorize?client_id=" + PUBLIC_HACKCLUB_AUTH + "&redirect_uri=" + PUBLIC_HACKCLUB_REDIRECT + "&response_type=code&scope=" + scopes + "&state=" + state
     return redirect(308, url)
 }
